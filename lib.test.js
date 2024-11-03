dfa7256ccaeb251f81196c1116a142c5705ac29d
park <dnjsrnrdl456@gmail.com>
2024-11-03 13:43:26 +0900

    Add test

--- lib.test.js
+++ lib.test.js
@@ -8,3 +8,11 @@ test("avg([3, 5, 6]) should be 5", () => {
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+
+test("prime(7) should be true", () => {
+    expect(lib.prime(7)).toBe(true);
+});
+
+test("factorial(5) should be 120", () => {
+    expect(lib.factorial(5)).toBe(120);
+});
